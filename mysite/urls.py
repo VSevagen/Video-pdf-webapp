b3d80b0182194e357fbb38230b3f5a490c221c52
sevagen <sevagenv@gmail.com>
2020-08-08 11:48:26 +0530

    Make upload from homepage

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,7 +7,7 @@ from mysite.core import views
 
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
+    path('',views.upload, name='upload' ),
     path('upload/', views.upload, name='upload'),
     path('books/', views.book_list, name='book_list'),
     path('books/upload/', views.upload_book, name='upload_book'),
