0a7f9d5b82c6eeb7d05ae3ba0bafd28a9a30656c
sevagen <sevagenv@gmail.com>
2020-08-16 13:56:00 +0530

    Complete duplicate eleiminating algorithm

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -33,6 +33,7 @@ def upload(request):
         name = fs.save(uploaded_file.name, uploaded_file)
         video_process(uploaded_file)
         removeDup()
+        raw_remove()
         makepdf()
         download(request)
         context['url'] = fs.url(name)
@@ -125,8 +126,32 @@ def removeDup():
     RemoveDupli(greyDir, Path)
     shutil.rmtree(greyDir)
 
-def makepdf():
+def raw_remove():
     path = os.getcwd()+"/images"
+    os.chdir(path)
+    myimages = []
+    dirFiles = os.listdir(os.getcwd())
+    fnames = sorted([fname for fname in os.listdir(os.getcwd()) if fname.endswith('.jpg')], key=lambda f: int(f.rsplit(os.path.extsep, 1)[0].rsplit(None,1)[-1]))
+
+    for(i, image) in enumerate (fnames):
+        for(i, imageCom)in enumerate (fnames):
+            if image == imageCom:
+                pass
+            else:
+                try:
+                    searchedImg = cv2.imread(image)
+                    ImgCompareGrey = cv2.imread(imageCom)
+                    FinalSearchedImg = cv2.cvtColor(searchedImg, cv2.COLOR_BGR2GRAY)
+                    FinalCompareImg = cv2.cvtColor(ImgCompareGrey, cv2.COLOR_BGR2GRAY)
+                    h = structural_similarity(FinalSearchedImg, FinalCompareImg)
+                except:
+                    continue
+                if h > 0.87:
+                    os.remove(image)
+                    print(image, imageCom, h)
+
+def makepdf():
+    path = os.getcwd()
     os.chdir(path)
     myimages = []
     dirFiles = os.listdir(os.getcwd())
